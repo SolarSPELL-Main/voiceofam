e5465060fcd02fd851cdf8ab83076417228f107d
alay1313 <artjj09@gmail.com>
2022-07-27 12:09:35 -0700

    removed solarSPELL logo sand changed icons

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ headerTemplate.innerHTML = `
 
 <header class="container-fluid">
 <nav class="navbar navbar-expand-lg navbar-light bg-blue" role="navigation">
-    <a class="navbar-brand img-fluid" href="/"><img class="nav-img" src="/voiceofam/assets/solarSPELL_logo.png" alt="SolarSPELL Logo"></img></a>
+    
     <a class="navbar-brand img-fluid" href="/voiceofam/index.html"><img class="nav-img" src="/voiceofam/assets/voa-logo.png"></img></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
